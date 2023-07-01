55b94dbb705cfff319d52abbfd0bb2c4e2b0d93f
amalkrsihna <amalkrsihna@ug.cusat.ac.in>
2023-07-01 19:25:24 +0530

    hello world program with function call

---
+++ function/001helloWorld/main.py
@@ -0,0 +1,13 @@
+
+# creating function ,name of function truthValueTest
+def helloWorld():
+    print("hello world ")
+
+def main():
+    helloWorld()
+
+
+
+if __name__ == "__main__":
+    main()
+
