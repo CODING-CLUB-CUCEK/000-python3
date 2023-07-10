dd1c0bc5996009a8ffde489c5a8c5e63d80f1701
amalkrsihna <amalkrsihna@ug.cusat.ac.in>
2023-07-10 20:10:16 +0530

    refactor(arithmetic-operators): getting to know arithmetic perators and functions ,hackerank

---
+++ Hackerrank/Arithmetic Operators/main.py
@@ -0,0 +1,25 @@
+
+def add(a,b):
+    return a+b
+def sub(a,b):
+    return a-b
+def multi(a,b):
+    return a*b
+
+
+
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print(add(a,b))
+    print(sub(a,b))
+    print(multi(a,b))
+
+
+
+""" Here is the explanation for the code above:
+1. We have defined three functions for add, sub and multi.
+2. In the main function, we have taken two inputs from the user.
+3. Then we have called the functions and printed the results of the function calls.
+4. Now, we will see how to use a module in another program. """
