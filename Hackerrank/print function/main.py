60a183b77c23c365cf98b0307ef612f46b219870
amalkrsihna <amalkrsihna@ug.cusat.ac.in>
2023-07-10 23:16:20 +0530

    refactor: use ''' end ''' to print in the same line
    eg:print(i,end="") o/p=> 0123...

---
+++ Hackerrank/print function/main.py
@@ -0,0 +1,10 @@
+if __name__ == '__main__':
+    n = int(input())
+    if n <=150 and n >=1:
+        i=0
+        for i in range (0,n):
+          print(i+1,end="")
+          #to print in the same line
+          
+           
+
